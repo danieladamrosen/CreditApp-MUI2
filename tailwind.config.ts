d6e12983dad0cf052f4831bfc069d059ed5ea5dc
danieladamrosen <dr@creditrepaircloud.com>
2025-06-29 02:26:24 -0700

    Backup-2025-06-29-Completed-Normalization: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ export default {
   content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Lato', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'sans-serif'],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
